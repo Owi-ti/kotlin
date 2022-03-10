a89e6ad504ce91abfa3870e7819f0effb5300b2b
Owi-ti <owittiesther@gmail.com>
2022-03-10 08:56:10 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,37 @@
+git import org.w3c.dom.Text
+
+fun main(){
+
+     var result=getRemainder(3,2)
+    println(result)
+     myDetails("Esther",20)
+
+
+
+
+}
+
+fun School(name:String){
+    var text= "CodeHive"
+    var Char0=text[4]
+    var Char2=text[5]
+    var Char3= text[7]
+    var total=text[4].toString()[5].toString()[7]
+
+
+
+}
+fun getRemainder(num1:Int,num2:Int) {
+    var num = num1 % num2
+    println(num)
+}
+fun myDetails(name:String,age:Byte){
+    var text= ("Hi, my name is $name and i am  $age years old")
+    println(text)
+
+}
+fun measureMent(name:String){
+    var name="Esther"
+    return
+    }
+
